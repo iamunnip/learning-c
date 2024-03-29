6e79640a325c0fe1a4ea1bdb4bbd72321e8ef576
Unni P <unnipofficial@outlook.com>
2024-03-29 19:41:30 +0530

    added programs for comments and format specification

---
+++ 01-basics/06-comments.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // Single line comment
+    printf("Single line comment\n");
+
+    /* Multi 
+    line 
+    comment */
+    printf("Multi line comment\n");
+    return 0;
+}
